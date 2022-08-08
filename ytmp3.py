2c5d1a18494e7cf8b0d27e0b0fe6c92cbfcfdb2d
Uday Badhe <74442221+udaybadhe@users.noreply.github.com>
2022-08-08 18:10:46 +0530

    Update ytmp3.py

--- ytmp3.py
+++ ytmp3.py
@@ -1,12 +1,8 @@
-#import os
-#from moviepy.editor import *
 from pytube import Search
-from pytube import YouTube
+import os
 def query_pars(a):
 	s = Search(a)
-	yt = YouTube(s.results[0].watch_url)
-	r = yt.streams.filter(only_audio=True)
-	s = yt.streams.get_by_itag(140)
-	s.download('D://uni')
+	link = s.results[0].watch_url
+	os.system('youtube-dl.exe -x --audio-format mp3 {}'.format(link))
 a = input("Enter the Song name: ")
 query_pars(a)
